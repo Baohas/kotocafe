b36bf375f63897886df0321c6d4451058e3149bd
Razvodyatel <obolenskiy.baohas@gmail.com>
2026-04-09 09:57:19 +0300

    80% complete dashboard Added DB link Completed session closing Completed confirm window

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,6 +1,12 @@
 function confirm(){
     container = document.getElementById("container");
     container.classList.add("blur");
-
-    console.log(container);
+    conf = document.getElementById("confirm");
+    conf.classList.remove("hidden");
 };
+function confirm_close(){
+    container = document.getElementById("container");
+    conf = document.getElementById("confirm");
+    container.classList.remove("blur");
+    conf.classList.add("hidden");
+}
